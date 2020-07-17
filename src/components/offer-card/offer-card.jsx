1997c77efdea057479eb71e968a97c85ef97a6bb
like-programmer <dashutkadevi@gmail.com>
2020-07-17 19:05:04 +0300

    3.5.1 create an offer card

---
+++ src/components/offer-card/offer-card.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const OfferCard = (props) => {
+  const {card} = props;
+
+  return (
+    <article
+      className="cities__place-card place-card"
+    >
+      {card.isPremium ?
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+        : ``}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={`img/${card.picture}`} width={260} height={200} alt={card.title}/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{card.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button
+            className={`place-card__bookmark-button ${card.isBookmarked ? `place-card__bookmark-button--active` : ``} button`}
+            type="button">
+            <svg className="place-card__bookmark-icon" width={18} height={19}>
+              <use xlinkHref="#icon-bookmark"/>
+            </svg>
+            <span className="visually-hidden">{card.isBookmarked ? `In bookmarks` : `To bookmarks`}</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: card.rating}}/>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{card.title}</a>
+        </h2>
+        <p className="place-card__type">{card.type}</p>
+      </div>
+    </article>
+  );
+};
+
+OfferCard.propTypes = {
+  card: PropTypes.object.isRequired,
+};
+
+export default OfferCard;
--- src/components/offer-list/offer-list.jsx
+++ src/components/offer-list/offer-list.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import OfferCard from "../offer-card/offer-card.jsx";
 
 const OfferList = (props) => {
   const {offers} = props;
 
   return (
-    <div className="cities__places-list places__list tabs__content"></div>
+    <div className="cities__places-list places__list tabs__content">
+
+      <OfferCard
+        card={offers[0]}
+      />
+
+    </div>
   );
 };
 
