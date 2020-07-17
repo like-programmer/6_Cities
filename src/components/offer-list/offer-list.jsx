e83f801cb1af2ffb65e023805c3e806e012c474e
like-programmer <dashutkadevi@gmail.com>
2020-07-17 17:56:29 +0300

    3.4 create an offer-list component

---
+++ src/components/offer-list/offer-list.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const OfferList = (props) => {
+  const {offers} = props;
+
+  return (
+    <p>JLJLFjSKLgjdflfjgdkfg</p>
+  );
+};
+
+OfferList.propTypes = {
+  offers: PropTypes.array.isRequired,
+};
+
+export default OfferList;
