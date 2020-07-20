48ffc74b190828a9091db00cfddc412aa0626c5d
like-programmer <dashutkadevi@gmail.com>
2020-07-20 14:59:48 +0300

    3.10 update tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,10 +2,22 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app.jsx";
 
+const offers = [
+  {
+    picture: `room.jpg`,
+    price: 140,
+    rating: `60%`,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    isBookmarked: false,
+    isPremium: false,
+  },
+];
+
 it(`Render App`, () => {
   const tree = renderer
     .create(<App
-      rentTitles={[`Wood and stone place`, `Canal View Prinsengracht`]}
+      offers={offers}
     />)
     .toJSON();
 
--- src/components/main-screen/main-screen.test.js
+++ src/components/main-screen/main-screen.test.js
@@ -2,11 +2,22 @@ import React from "react";
 import renderer from "react-test-renderer";
 import MainScreen from "./main-screen.jsx";
 
+const offers = [
+  {
+    picture: `room.jpg`,
+    price: 140,
+    rating: `60%`,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    isBookmarked: false,
+    isPremium: false,
+  },
+];
+
 it(`Should MainScreen render correctly`, () => {
   const tree = renderer
     .create(<MainScreen
-      rentTitles={[`Wood and stone place`, `Canal View Prinsengracht`]}
-      rentCardClickHandler={() => {}}
+      offers={offers}
     />)
     .toJSON();
 
