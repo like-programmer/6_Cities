1381ef5c5b2113bbe7edc1e505662e884b3a2d8d
like-programmer <dashutkadevi@gmail.com>
2020-07-29 22:02:25 +0300

    4.11 create review-list & review-item components

--- src/components/offer-details-screen/offer-details-screen.jsx
+++ src/components/offer-details-screen/offer-details-screen.jsx
@@ -34,7 +34,6 @@ const OfferDetailsScreen = (props) => {
         <section className="property">
 
           <div className="property__gallery-container container">
-
             <div className="property__gallery">
               <div className="property__image-wrapper">
                 <img className="property__image" src="img/room.jpg" alt="Photo studio"/>
@@ -55,7 +54,6 @@ const OfferDetailsScreen = (props) => {
                 <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
               </div>
             </div>
-
           </div>
 
           <div className="property__container container">
