0b2d910962e59f75ce2db08b4c8cebfd95a8fb77
like-programmer <dashutkadevi@gmail.com>
2020-08-07 15:52:42 +0300

    5.1 install redux & react-redux, describe an initial structure (reducer, store)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
 import offers from "./mocks/offers.js";
 import reviews from "./mocks/reviews.js";
+import {reducer} from "./reducer.js";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App
-      offers={offers}
-      reviews={reviews}
-    />,
+    <Provider store={store}>
+      <App
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {};
+
+const ActionType = {};
+
+const reducer = (state = initialState, actiob) => {
+  // switch (action.type) {
+    // case ActionType. :
+    //   return
+  // }
+
+  return state;
+};
+
+export {reducer, ActionType};
