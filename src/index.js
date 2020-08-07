f23a8976d10d672d3985b1c6da78e9f1b152ca16
like-programmer <dashutkadevi@gmail.com>
2020-08-07 16:10:33 +0300

    5.1.1 add redux DevTools connection

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import offers from "./mocks/offers.js";
 import reviews from "./mocks/reviews.js";
 import {reducer} from "./reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
