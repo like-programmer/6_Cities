9e01208f60618b2966fce0a6509a36ab5743f9da
like-programmer <dashutkadevi@gmail.com>
2020-08-13 21:50:08 +0300

    6.2 move page header into new component & connect it to the main page & offer-details-screen

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {ActionCreator} from "../../reducer.js";
+import PageHeader from "../page-header/page-header.jsx";
 import CitiesList from "../cities-list/cities-list.jsx";
 import Sorting from "../sorting/sorting.jsx";
 import OfferList from "../offer-list/offer-list.jsx";
@@ -29,28 +30,8 @@ const MainScreen = (props) => {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41}/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+
+      <PageHeader/>
 
       <main className={`page__main page__main--index ${offers.length === 0 ? `page__main--index-empty` : ``}`}>
         <h1 className="visually-hidden">Cities</h1>
@@ -67,6 +48,7 @@ const MainScreen = (props) => {
         </div>
 
         <div className="cities">
+
           {offers.length === 0 ? <NoOffers cityName={activeCity}/> :
             <div className="cities__places-container container">
               <section className="cities__places places">
@@ -99,6 +81,7 @@ const MainScreen = (props) => {
               </div>
             </div>
           }
+
         </div>
       </main>
     </div>
